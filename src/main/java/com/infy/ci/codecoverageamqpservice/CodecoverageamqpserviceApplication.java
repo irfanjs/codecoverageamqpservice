979783e338304e47111c7cec1ef6fffd3869c63a
Irfan Sayed <irfan.sayed@ad.infosys.com>
2018-06-03 13:25:01 +0200

    initial commit

---
+++ src/main/java/com/infy/ci/codecoverageamqpservice/CodecoverageamqpserviceApplication.java
@@ -0,0 +1,12 @@
+package com.infy.ci.codecoverageamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoverageamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecoverageamqpserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infy/ci/codecoverageamqpservice/CodecoverageamqpserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infy.ci.codecoverageamqpservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodecoverageamqpserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
