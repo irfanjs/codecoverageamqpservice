efdaf0d3e4acc17d8e413d956f11d1eae0847e93
Sayed <Irfan.Sayed@Honeywell.com>
2019-06-30 14:36:21 +0530

    code modify

--- src/main/java/com/infy/ci/codecoverageamqpservice/CodeCovAmqpRestController.java
+++ src/main/java/com/infy/ci/codecoverageamqpservice/CodeCovAmqpRestController.java
@@ -2,67 +2,44 @@ package com.infy.ci.codecoverageamqpservice;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.concurrent.TimeoutException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 
 @Controller
-@PropertySource("classpath:application.properties")
+@Component
 @Configuration
 @RequestMapping("/codecoveragedata")
 public class CodeCovAmqpRestController {
-	private String replyQueueName;
-
-	private Connection connection;
-	private Channel channel;
-	private String requestQueueName = "rpc_queue_codecover";
-
-	private final Logger logger = LoggerFactory.getLogger(CodeCovAmqpRestController.class);
-	private RabbitTemplate rabbitTemplate;
-
-	@Value("${spring.rabbitmq.host}")
-	private String host;
-
-	@Autowired
-	public CodeCovAmqpRestController() {
-
-	}
-
-	@Autowired
-	public void setAllConnection(RabbitTemplate rabbitTemplate) throws IOException, TimeoutException {
-
-		this.rabbitTemplate = rabbitTemplate;
-		rabbitTemplate.setReplyTimeout(15_000L);
-
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost(host);
-		connection = factory.newConnection();
-		channel = connection.createChannel();
-		logger.info("The rabbitMQ host is :  " + host);
-	}
 	
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+	
+	@Value("${jsa.rabbitmq.exchange}")
+	   private String exchange;
+	   
+	   @Value("${jsa.rabbitmq.routingkey}")
+	   private String routingKey;
+	   
+	   private final Logger logger = LoggerFactory.getLogger(CodeCovAmqpRestController.class);
+	   
 	@RequestMapping(value = "/{projectid}/cc/aggregate", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
 	public @ResponseBody String getAggregatedDataForSectionOfNightlyBuild(@PathVariable("projectid") int projectid,
 			@RequestParam("buildtype") String buildtype, @RequestParam("build") String build) throws Exception {
 		String message = String.format("aggregate" + "-" + projectid + "-" + build + "-" + buildtype);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for aggregate");
@@ -76,7 +53,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("modulewise" + "-" + projectid + "-" + build + "-" + buildtype);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for modulewise");
@@ -89,7 +66,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("week" + "-" + projectid);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for week");
@@ -102,7 +79,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("month" + "-" + projectid);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for month");
@@ -116,7 +93,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("custom" + "-" + projectid + "-" + todate + "-" + fromdate);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for custom");
@@ -131,7 +108,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("projects" + "-" + projectid);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for projects");
@@ -146,7 +123,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("latestnightlybuilds" + "-" + projectid);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for latestnightlybuilds");
@@ -162,7 +139,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("daterange" + "-" + projectid);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for daterange");
@@ -177,7 +154,7 @@ public class CodeCovAmqpRestController {
 
 		String message = String.format("buildnumber" + "-" + projectid + "-" + buildnumber);
 		logger.info("Sending: " + message);
-		Object returned = rabbitTemplate.convertSendAndReceive("", requestQueueName, message);
+		Object returned = amqpTemplate.convertSendAndReceive(exchange, routingKey, message);
 		logger.info("Reply: " + returned);
 		if (returned == null) {
 			throw new RuntimeException("failed to get a response for buildnumber");
@@ -186,7 +163,5 @@ public class CodeCovAmqpRestController {
 
 	}
 
-	public void close() throws IOException {
-		connection.close();
-	}
+	
 }
